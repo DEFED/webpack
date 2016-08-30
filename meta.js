3e0d5afc2c21c4a6f8133e5da9b57f6461bf2513
maxchenliang <maxchenliang@didichuxing.com>
2016-08-30 11:10:15 +0800

    bug

--- meta.js
+++ meta.js
@@ -10,11 +10,15 @@
             "required": true,
             "message": "组件中文名称，如：选择器"
         },
+        "author": {
+            "type": "string",
+            "message": "Author"
+        },
         "demoUrl": {
             "type": "string",
             "required": false,
             "message": "组件的demo地址，现在没有，但之后最好补上。demo由组件开发者自行维护"
-        },
-        "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev"
-    }
+        }
+    },
+    "completeMessage": "To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 }
